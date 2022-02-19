dd03f5edf2c013faca11723ba6dbbd0a98c70046
Henrique Balmant <henrique.balmant@gmail.com>
2022-02-19 19:08:05 -0300

    feat: add express async error to deal with exceptions

--- server.ts
+++ server.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof Error) {
+    return response.status(400).json({
+      message: error.message
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  });
+});
 
 app.listen(3000, () => console.log("🚀 Server is running on port 3000..."));
