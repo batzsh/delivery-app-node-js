9c9557410d0de7be5a77be2d2dd2181d08ac4839
Henrique Balmant <henrique.balmant@gmail.com>
2022-02-19 15:44:06 -0300

    feat: create client controller

---
+++ src/infra/controllers/create-client-controller.ts
@@ -0,0 +1,16 @@
+import { CreateClientUseCase } from "@domains/clients/usecases/create-client-use-case";
+import { Request, Response } from "express";
+
+export class CreateClientController {
+  async handle(request: Request, response: Response) {
+    const { username, password } = request.body;
+
+    const createClientUseCase = new CreateClientUseCase();
+    const result = await createClientUseCase.execute({
+      username,
+      password
+    });
+
+    return response.json(result);
+  }
+}
