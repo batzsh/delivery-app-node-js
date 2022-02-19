73cea15070c19e908b6d2a36f72cb1df56db02cd
Henrique Balmant <henrique.balmant@gmail.com>
2022-02-19 15:41:56 -0300

    feat: create client use case

---
+++ src/domains/clients/usecases/create-client-use-case.ts
@@ -0,0 +1,34 @@
+import { prisma } from "@infra/prisma/prisma-client";
+import { hash } from "bcrypt";
+
+export class CreateClientUseCase {
+  async execute({ username, password }: CreateClientUseCase.Request) {
+    const clientExists = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: "insensitive"
+        }
+      }
+    });
+
+    if (clientExists) {
+      throw new Error("Client already exists");
+    }
+
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: await hash(password, 10)
+      }
+    });
+
+    return client;
+  }
+}
+
+export namespace CreateClientUseCase {
+  export interface Request {
+    username: string;
+    password: string;
+  }
+}
