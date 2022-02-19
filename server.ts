fb4025727be0c71be5b2c9f657693140def234b6
Henrique Balmant <henrique.balmant@gmail.com>
2022-02-19 15:37:26 -0300

    feat: add server init

---
+++ server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({
+    message: "Hello world"
+  });
+});
+
+app.listen(3000, () => console.log("🚀 Server is running on port 3000..."));
